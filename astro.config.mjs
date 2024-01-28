56dc4860e79ceffc2a57cd6d72b068c9412097e6
Diego Tejada <diegotejada@Diegos-MacBook-Pro.local>
2024-01-28 18:15:21 -0500

    Test base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://thattejada.github.io',
-	// base: '',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
