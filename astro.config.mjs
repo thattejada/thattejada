5ef263eec8bb169a6b6fb626db3764cf1c6ab52f
Diego Tejada <diegotejada@Diegos-MacBook-Pro.local>
2024-01-28 17:53:49 -0500

    Set Github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://thattejada.github.io',
+	// base: '',
 	integrations: [mdx(), sitemap()],
 });
